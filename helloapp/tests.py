48774d68ee01cde4da6752a8cce0c8b869e36628
steve nagle <92252275+ellipsis52@users.noreply.github.com>
2023-10-09 19:25:30 +0200

    validation

--- helloapp/tests.py
+++ helloapp/tests.py
@@ -15,3 +15,10 @@ class ViewTests(SimpleTestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'This is an example')
         self.assertContains(resp, 'Home')
+web: python3 -m ptvsd --port 3000 --host 0.0.0.0 manage.py runserver 0.0.0.0:8080 --noreload
+source /Users/webtechnicom/PycharmProjects/venv/bin/activate
+(base) webtechnicom@MacBook-Pro-de-steve hello-world-2 % source /Users/webtechnicom/PycharmP
+rojects/venv/bin/activate
+(venv) (base) webtechnicom@MacBook-Pro-de-steve hello-world-2 % >....                       
+        echo "Expected content found -- site is up"
+        echo "END CONTENT TEST: Success! ✅"
--- helloproject/asgi.py
+++ helloproject/asgi.py
@@ -14,3 +14,10 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloproject.settings')
 
 application = get_asgi_application()
+web: python3 -m ptvsd --port 3000 --host 0.0.0.0 manage.py runserver 0.0.0.0:8080 --noreload
+source /Users/webtechnicom/PycharmProjects/venv/bin/activate
+(base) webtechnicom@MacBook-Pro-de-steve hello-world-2 % source /Users/webtechnicom/PycharmP
+rojects/venv/bin/activate
+(venv) (base) webtechnicom@MacBook-Pro-de-steve hello-world-2 % >....                       
+        echo "Expected content found -- site is up"
+        echo "END CONTENT TEST: Success! ✅"
